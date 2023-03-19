811c9a758fad4961f73f122c41aba268e836b3bd
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-03-19 11:27:52 +0600

    routes defined

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { Route, Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Pages/Blogs/Blogs';
 import Home from './Pages/Home/Home';
+import Services from './Pages/Services/Services';
 
 function App() {
   return (
     <div className="App">
       
-      <Router>
+      <Routes>
         <Route path='/' element={<Home />} />
-      </Router>
+        <Route path='services' element={<Services />} />
+        <Route path='blogs' element={<Blogs />} />
+      </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
+  content: [ "./src/**/*.{js,jsx,ts,tsx}", ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {     
+        "primary": "#FF574B",
+        "secondary": "#FF556B",
+        "accent": "#1FB2A6", 
+        "neutral": "#191D24",   
+        "base-100": "#ffff",
+        "info": "#3ABFF8",
+        "success": "#36D399",
+        "warning": "#FBBD23",
+        "error": "#F87272",
+        },
+      },
+    ],
   },
-  plugins: [],
-};
+  plugins: [require("daisyui")]
+}

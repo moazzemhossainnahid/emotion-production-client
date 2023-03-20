ae827529abb645965d19955f32d46786b8b0eb7c
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-03-20 22:16:29 +0600

    why emotion part header design

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../../Components/Home/Banner';
+import WhyEmotion from '../../Components/Home/WhyEmotion/WhyEmotion';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <WhyEmotion/>
         </div>
     );
 };

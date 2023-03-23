57b1bc99b518f41c27c27547daaf82aa36653580
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-03-23 14:15:04 +0600

    add trusted by

--- src/Components/Home/TrustedBy/TrustedBy.jsx
+++ src/Components/Home/TrustedBy/TrustedBy.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 
 const TrustedBy = () => {
     return (
-        <div className='pb-10'>
+        <div className='container p-3 md:p-1 w-full md:w-5/6 mx-auto pb-10'>
             <h3 className="text-2xl md:text-3xl text-white font-bold">Trusted By</h3>
-
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-3 py-10">
+                <div className="bg-[#707070] rounded flex justify-center items-center px-7 py-10">
+                    <img src="https://svgshare.com/i/rN9.svg" alt="" className="object-cover" />
+                </div>
+                <div className="bg-[#707070] rounded flex justify-center items-center px-7 py-10">
+                    <img src="https://svgshare.com/i/rMS.svg" alt="" className="object-cover" />
+                </div>
+                <div className="bg-[#707070] rounded flex justify-center items-center px-7 py-10">
+                    <img src="https://svgshare.com/i/rP3.svg" alt="" className="object-cover" />
+                </div>
+                <div className="bg-[#707070] rounded flex justify-center items-center px-7 py-10">
+                    <img src="https://svgshare.com/i/rNh.svg" alt="" className="object-cover" />
+                </div>
+            </div>
         </div>
     );
 };

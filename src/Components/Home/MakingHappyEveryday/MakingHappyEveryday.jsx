d13cffb273182d41898695041553eeec548f1e2a
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-03-25 23:24:42 +0600

    add bg

--- src/Components/Home/MakingHappyEveryday/MakingHappyEveryday.jsx
+++ src/Components/Home/MakingHappyEveryday/MakingHappyEveryday.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const MakingHappyEveryday = () => {
     return (
-        <div>
-            
+        <div className='bg-[#707070]'>
+            <img src="https://svgshare.com/i/rSu.svg" alt="" className="w-full h-full -p-5 opacity-5 object-cover" />
         </div>
     );
 };

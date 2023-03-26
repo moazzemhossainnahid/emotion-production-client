3d30f07058e2871aba702639254c95176cee28c1
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-03-27 00:49:23 +0600

    blog data

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div className='container py-10'>
-            <div className="">
             <h3 className="text-2xl md:text-3xl text-white font-bold pb-10">Blogs</h3>
             <p className="text-white">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.consetetur sadipscing elitr</p>
+            <div className="">
+
             </div>
         </div>
     );

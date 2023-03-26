ce9650b8104a8778f3dea9722649a33a0e33172c
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-03-26 22:40:58 +0600

    blog page add

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            
+        <div className='container py-10'>
+            <div className="">
+            <h3 className="text-2xl md:text-3xl text-white font-bold pb-10">Blogs</h3>
+            <p className="text-white">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.consetetur sadipscing elitr</p>
+            </div>
         </div>
     );
 };

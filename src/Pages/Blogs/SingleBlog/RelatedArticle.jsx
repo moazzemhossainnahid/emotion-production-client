9963ce6e23187df1491ecf3283bb0890fb193d3a
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-03-27 02:20:52 +0600

    more blog add

---
+++ src/Pages/Blogs/SingleBlog/RelatedArticle.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useNavigate } from 'react-router';
+import data from '../../../Data/BlogsData.json';
+
+const RelatedArticle = ({blog}) => {
+    const navigate = useNavigate();
+
+    const disMatchId = data?.filter(cr => cr?.id !== blog?.id);
+    // console.log(disMatchId);
+
+    return (
+        <div className='container'>
+            <h3 className="text-2xl md:text-3xl text-white text-start font-bold pb-10">Related Article</h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+                {
+                    disMatchId?.slice(0, 3)?.map(blog => (
+                        <div onClick={() => navigate(`/blogs/${blog?.id}`)} key={blog?.id} className="group">
+                            <div className='w-full flex flex-col gap-3 mx-auto py-7'>
+                                <div className="w-full mx-auto h-52 overflow-hidden">
+                                    <img src={blog?.blogImg} alt="" className="object-cover group-hover:scale-105 group-hover:duration-300" />
+                                </div>
+                                <div className="w-full mx-auto text-start space-y-3 px-3">
+                                    <h3 className="text-xl font-bold text-white">{blog?.title}</h3>
+                                    <div className="flex justify-between items-center gap-3">
+                                        <div className="flex justify-start items-center gap-3 pt-3">
+                                            <img src={blog?.authorImg} alt="" className="w-8 h-8 rounded-full" />
+                                            <p className="text-white">By {blog?.author}</p>
+                                        </div>
+                                        <span className="bg-[#707070] text-white rounded-3xl px-5 py-2">{blog?.badge}</span>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    );
+};
+
+export default RelatedArticle;
--- src/Pages/Blogs/SingleBlog/SingleBlog.jsx
+++ src/Pages/Blogs/SingleBlog/SingleBlog.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router';
 import data from '../../../Data/BlogsData.json';
+import RelatedArticle from './RelatedArticle';
 
 
 const SingleBlog = () => {
@@ -11,25 +12,29 @@ const SingleBlog = () => {
 
     return (
         <div className='container py-10 w-full md:w-5/6 mx-auto'>
-            <div className="pb-3">
-                <h3 className="text-2xl md:text-4xl text-white text-start py-2">{blog?.title}</h3>
-                <div className="flex w-full md:w-1/3 justify-between items-center gap-3">
-                    <div className="flex justify-start items-center gap-3 pt-3">
-                        <img src={blog?.authorImg} alt="" className="w-8 h-8 rounded-full" />
-                        <p className="text-white">By {blog?.author}</p>
+            <div className="">
+                <div className="pb-3">
+                    <h3 className="text-2xl md:text-4xl text-white text-start py-2">{blog?.title}</h3>
+                    <div className="flex w-full md:w-1/3 justify-between items-center gap-3">
+                        <div className="flex justify-start items-center gap-3 pt-3">
+                            <img src={blog?.authorImg} alt="" className="w-8 h-8 rounded-full" />
+                            <p className="text-white">By {blog?.author}</p>
+                        </div>
+                        <span className="bg-[#707070] text-white rounded-3xl px-5 py-2">{blog?.badge}</span>
                     </div>
-                    <span className="bg-[#707070] text-white rounded-3xl px-5 py-2">{blog?.badge}</span>
+                </div>
+                <div className="space-y-5">
+                    <div className="w-full md:w-5/6 h-96 py-7">
+                        <img src={blog?.blogImg} alt="" className="object-cover w-full rounded-t-3xl h-96" />
+                    </div>
+                    <div className="w-full py-7">
+                        <p className="text-[#C9C9C9]">{blog?.blogText}</p>
+                    </div>
                 </div>
             </div>
-            <div className="space-y-5">
-                <div className="w-full md:w-5/6 h-96 py-7">
-                    <img src={blog?.blogImg} alt="" className="object-cover w-full rounded-t-3xl h-96" />
-                </div>
-                <div className="w-full py-7">
-                    <p className="text-[#C9C9C9]">{blog?.blogText}</p>
-                </div>
+            <div className="py-10">
+                <RelatedArticle blog={blog} />
             </div>
-
         </div>
     );
 };

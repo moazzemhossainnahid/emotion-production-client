9868f460d9d6f68ee23abb8e9108e3a298af7b3b
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-03-26 13:14:50 +0600

    change

--- src/Components/Home/MakingHappyEveryday/MakingHappyEveryday.jsx
+++ src/Components/Home/MakingHappyEveryday/MakingHappyEveryday.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const MakingHappyEveryday = () => {
     return (
-        <div className='bg-[#707070]'>
-            <img src="https://svgshare.com/i/rSu.svg" alt="" className="w-full h-full -p-5 opacity-5 object-cover" />
+        <div className='bg-[#1E1E1E]'>
+            <div className="bg-[url('https://svgshare.com/i/rUV.svg')] relative rounded-3xl flex items-center w-full mx-auto h-[80vh] md:h-[75vh] bg-cover p-7">
+                <h3 className="text-center text-white text-3xl font-semibold">Making Many Happy Everyday </h3>
+
+            </div>
         </div>
     );
 };

3b30bc42795e93bfa5ca7333c151bc2c04997e3b
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-03-27 01:56:44 +0600

    image setup

--- src/Pages/Blogs/SingleBlog/SingleBlog.jsx
+++ src/Pages/Blogs/SingleBlog/SingleBlog.jsx
@@ -11,15 +11,22 @@ const SingleBlog = () => {
 
     return (
         <div className='container py-10 w-full md:w-5/6 mx-auto'>
-            <h3 className="text-2xl md:text-4xl text-white text-start py-2">{blog?.title}</h3>
-            <div className="flex w-full md:w-1/3 justify-between items-center gap-3">
-                <div className="flex justify-start items-center gap-3 pt-3">
-                    <img src={blog?.authorImg} alt="" className="w-8 h-8 rounded-full" />
-                    <p className="text-white">By {blog?.author}</p>
+            <div className="pb-3">
+                <h3 className="text-2xl md:text-4xl text-white text-start py-2">{blog?.title}</h3>
+                <div className="flex w-full md:w-1/3 justify-between items-center gap-3">
+                    <div className="flex justify-start items-center gap-3 pt-3">
+                        <img src={blog?.authorImg} alt="" className="w-8 h-8 rounded-full" />
+                        <p className="text-white">By {blog?.author}</p>
+                    </div>
+                    <span className="bg-[#707070] text-white rounded-3xl px-5 py-2">{blog?.badge}</span>
                 </div>
-                <span className="bg-[#707070] text-white rounded-3xl px-5 py-2">{blog?.badge}</span>
-
             </div>
+            <div className="">
+                <div className="w-full md:w-5/6 h-96 py-7">
+                    <img src={blog?.blogImg} alt="" className="object-cover w-full rounded-t-3xl h-96" />
+                </div>
+            </div>
+
         </div>
     );
 };

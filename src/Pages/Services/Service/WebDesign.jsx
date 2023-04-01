0125f61fd210b5e7beb9f92c29d706efb19a3695
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-04-01 23:23:08 +0600

    card anim

--- src/Pages/Services/Service/WebDesign.jsx
+++ src/Pages/Services/Service/WebDesign.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ProgressBar from 'react-animated-progress-bar';
 import { PalnsData } from '../../../Data/PlansData';
 // import '../../style.css'
+import Fade from 'react-reveal/Fade';
 
 const WebDesign = () => {
 
@@ -193,55 +194,63 @@ const WebDesign = () => {
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 justify-around items-center py-10'>
                     {
                         PalnsData?.map(plan => (
-                            <div key={plan?.id} className="p-5 py-10 text-white bg-[#1E1E1E] hover:shadow-2xl duration-200 rounded-xl md:mx-5">
-                                <h3 className="text-2xl py-5">{plan?.plan}</h3>
-                                <h2 className="text-5xl md:text-6xl font-bold py-5">${plan?.price} <sub className='text-xl -ml-3'>/mo</sub> </h2>
-                                <div className="space-y-3 pt-5">
-                                    {
-                                        plan?.data?.map(d => (
-                                            <p key={d} className="">{d}</p>
-                                        ))
-                                    }
-                                </div>
+                            <Fade bottom>
+                                <div key={plan?.id} className="p-5 py-10 text-white bg-[#1E1E1E] hover:shadow-2xl duration-200 rounded-xl md:mx-5">
+                                    <h3 className="text-2xl py-5">{plan?.plan}</h3>
+                                    <h2 className="text-5xl md:text-6xl font-bold py-5">${plan?.price} <sub className='text-xl -ml-3'>/mo</sub> </h2>
+                                    <div className="space-y-3 pt-5">
+                                        {
+                                            plan?.data?.map(d => (
+                                                <p key={d} className="">{d}</p>
+                                            ))
+                                        }
+                                    </div>
 
-                                <div className="pt-5">
-                                    <button className='px-10 py-3 rounded-3xl text-white border hover:border-primary border-white hover:shadow-xl'>Get Started</button>
+                                    <div className="pt-5">
+                                        <button className='px-10 py-3 rounded-3xl text-white border hover:border-primary border-white hover:shadow-xl'>Get Started</button>
+                                    </div>
                                 </div>
-                            </div>
+                            </Fade>
                         ))
                     }
                 </div>
             </div>
             <div className='flex flex-col md:flex-row justify-around items-center mt-24 mb-36 w-full md:w-5/6 mx-auto'>
-                <div className='bg-[#313741] p-6 space-y-3'>
-                    <h2 className="text-3xl font-bold mb-3 text-left text-black">02.</h2>
-                    {/* <h4 className="text-5xl text-left mb-3 text-white font-black">WEB DESIGN</h4> */}
-                    <span className="flex justify-between items-center">
-                        <h4 className="text-2xl text-[#05C7AE] font-semibold mb-3 text-left">WEB DESIGN</h4>
-                        {/* <img className='w-24' src="https://i.ibb.co/1LcT7sF/download-removebg-preview.png" alt="" /> */}
-                    </span>
-                    <p className="inline-block align-middle text-start pb-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus laoreet dolor metus, eu ullamcorper turpis ornare tincidunt. Vivamus tristique rhoncus enim. Curabitur varius est quis din pat.
-                    </p>
-                </div>
-                <div className='bg-[#05C7AE] p-6 py-10 space-y-3'>
-                    <h2 className="text-3xl font-bold mb-3 text-left text-[#07463E]">01.</h2>
-                    {/* <h4 className="text-5xl text-left mb-3 text-white font-black">WEB DESIGN</h4> */}
-                    <span className="flex justify-between items-center">
-                        <h4 className="text-2xl text-black font-semibold mb-3 text-left">UX/UI DESIGN</h4>
-                        {/* <img className='w-24' src="https://i.ibb.co/1LcT7sF/download-removebg-preview.png" alt="" /> */}
-                    </span>
-                    <p className="inline-block align-middle text-start pb-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus laoreet dolor metus, eu ullamcorper turpis ornare tincidunt. Vivamus tristique rhoncus enim. Curabitur varius est quis din pat.
-                    </p>
-                </div>
-                <div className='bg-[#313741] p-6 space-y-3'>
-                    <h2 className="text-3xl text-left font-bold mb-3  text-black">03.</h2>
-                    <span className="flex justify-between items-center">
-                        <h4 className="text-2xl text-[#05C7AE] font-semibold mb-3 text-left">APP DESIGN</h4>
-                        {/* <img className='w-16' src="https://i.ibb.co/7XBNx6k/5088-removebg-preview.png" alt="" /> */}
-                    </span>
-                    <p className="inline-block align-middle text-start pb-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus laoreet dolor metus, eu ullamcorper turpis ornare tincidunt. Vivamus tristique rhoncus enim. Curabitur varius est quis din pat.
-                    </p>
-                </div>
+                <Fade bottom>
+                    <div className='bg-[#313741] p-6 space-y-3'>
+                        <h2 className="text-3xl font-bold mb-3 text-left text-black">02.</h2>
+                        {/* <h4 className="text-5xl text-left mb-3 text-white font-black">WEB DESIGN</h4> */}
+                        <span className="flex justify-between items-center">
+                            <h4 className="text-2xl text-[#05C7AE] font-semibold mb-3 text-left">WEB DESIGN</h4>
+                            {/* <img className='w-24' src="https://i.ibb.co/1LcT7sF/download-removebg-preview.png" alt="" /> */}
+                        </span>
+                        <p className="inline-block align-middle text-start pb-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus laoreet dolor metus, eu ullamcorper turpis ornare tincidunt. Vivamus tristique rhoncus enim. Curabitur varius est quis din pat.
+                        </p>
+                    </div>
+                </Fade>
+                <Fade top>
+                    <div className='bg-[#05C7AE] p-6 py-10 space-y-3'>
+                        <h2 className="text-3xl font-bold mb-3 text-left text-[#07463E]">01.</h2>
+                        {/* <h4 className="text-5xl text-left mb-3 text-white font-black">WEB DESIGN</h4> */}
+                        <span className="flex justify-between items-center">
+                            <h4 className="text-2xl text-black font-semibold mb-3 text-left">UX/UI DESIGN</h4>
+                            {/* <img className='w-24' src="https://i.ibb.co/1LcT7sF/download-removebg-preview.png" alt="" /> */}
+                        </span>
+                        <p className="inline-block align-middle text-start pb-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus laoreet dolor metus, eu ullamcorper turpis ornare tincidunt. Vivamus tristique rhoncus enim. Curabitur varius est quis din pat.
+                        </p>
+                    </div>
+                </Fade>
+                <Fade bottom>
+                    <div className='bg-[#313741] p-6 space-y-3'>
+                        <h2 className="text-3xl text-left font-bold mb-3  text-black">03.</h2>
+                        <span className="flex justify-between items-center">
+                            <h4 className="text-2xl text-[#05C7AE] font-semibold mb-3 text-left">APP DESIGN</h4>
+                            {/* <img className='w-16' src="https://i.ibb.co/7XBNx6k/5088-removebg-preview.png" alt="" /> */}
+                        </span>
+                        <p className="inline-block align-middle text-start pb-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus laoreet dolor metus, eu ullamcorper turpis ornare tincidunt. Vivamus tristique rhoncus enim. Curabitur varius est quis din pat.
+                        </p>
+                    </div>
+                </Fade>
             </div>
             {/*======== Portfulio =============== */}
             <div className='grid grid-cols-1 sm:grid-cols-3 place-items-center mb-36 gap-4'>

40177098db7fcca7e73720ec7789d3bc4476ad7b
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-04-04 14:21:04 +0600

    web design resolve

--- src/Pages/Services/Service/WebDesign.jsx
+++ src/Pages/Services/Service/WebDesign.jsx
@@ -1,58 +1,72 @@
 import React from 'react';
-import ProgressBar from 'react-animated-progress-bar';
 import { PalnsData } from '../../../Data/PlansData';
 import Fade from 'react-reveal/Fade';
-import { ProgressBarComponent } from '@syncfusion/ej2-react-progressbar';
 import Progress from '../../../Components/Others/Progress';
+import Bounce from 'react-reveal/Bounce';
+import Slide from 'react-reveal/Slide';
 
 
 const WebDesign = () => {
-    // const myAppRef = document.querySelector('.scrollable-div')
+
     return (
         <div className="container px-7 mx-auto w-full text-white ">
 
             <div className='flex flex-col md:flex-row justify-between items-center  mt-10 mb-24'>
-                <div className='w-full p-4'>
-                    <h2 className='text-center text-2xl sm:text-5xl sm:text-left'> Web Design can make easy your complex product.
-                        services</h2>
-                    <p className='inline-block  align-middle p-4 text-justify sm:text-left pt-3'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown print took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.
-                    </p>
-                    <p className='inline-block  align-middle p-4 text-justify sm:text-left pt-3'> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown print took a galley of type and scrambled electronic.
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown print took a galley of type and scrambled electronic.
-                    </p>
-                </div>
-                <div className='w-full p-4'>
-                    <img className=' w-full' src="https://i.ibb.co/sJ0chV2/ezgif-4-492a554f96.jpg" alt="" />
-                </div>
+                <Bounce bottom>
+                    <div className='w-full p-4 space-y-3'>
+                        <h2 className='text-center text-2xl sm:text-4xl sm:text-left'> Web Design can make easy your complex product.
+                            services</h2>
+                        <p className='inline-block  align-middle p-4 text-justify sm:text-left pt-3'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown print took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.
+                        </p>
+                        <p className='inline-block  align-middle p-4 text-justify sm:text-left pt-3'> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown print took a galley of type and scrambled electronic.
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown print took a galley of type and scrambled electronic.
+                        </p>
+                    </div>
+                </Bounce>
+                <Bounce bottom>
+                    <div className='w-full p-4'>
+                        <img className=' w-full' src="https://i.ibb.co/sJ0chV2/ezgif-4-492a554f96.jpg" alt="" />
+                    </div>
+                </Bounce>
             </div>
 
-            <div>
-                <h2 className=' sm:text-center text-2xl sm:text-5xl text-left'>Why Choose Me? </h2>
-                <p className="inline-block align-middle p-4 text-justify sm:text-left pt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown print took a galley of type and scrambled electronic.</p>
+            <div className='container px-7 mx-auto w-full text-white '>
+                <Slide bottom>
+                    <h2 className=' sm:text-center text-2xl sm:text-5xl text-left'>Why Choose Me? </h2>
+                </Slide>
+                <Fade>
+                    <p className="inline-block align-middle p-4 text-justify md:text-center pt-7">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown print took a galley of type and scrambled electronic.</p>
+                </Fade>
+
                 <div className='flex  flex-col md:flex-row justify-between items-center mt-7'>
                     <div>
-                        <div className='flex flex-col md:flex-row items-center justify-between mb-5 '>
-                            <h2 className='text-4xl'>Leadership</h2>
-                            <p className="inline-block align-middle p-4 text-justify text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum suscipit, temporibus, tempora nobis deleniti fuga sunt quia unde voluptates doloribus culpa rerum? Facere cupiditate culpa magnam animi corrupti laborum possimus.</p>
-                        </div>
-
-
-                        <div className='flex  flex-col md:flex-row items-center justify-between mb-5'>
-                            <h2 className='text-4xl '>Team,Work</h2>
-                            <p className="inline-block align-middle p-4 text-justify text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum suscipit, temporibus, tempora nobis deleniti fuga sunt quia unde voluptates doloribus culpa rerum? Facere cupiditate culpa magnam animi corrupti laborum possimus.</p>
-                        </div>
-
-                        <div className='flex  flex-col md:flex-row items-center justify-between'>
-                            <h2 className='text-4xl'>Flexibility</h2>
-                            <p className=" inline-block align-middle p-4 text-justify ml-0 sm:ml-6 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum suscipit, temporibus, tempora nobis deleniti fuga sunt quia unde voluptates doloribus culpa rerum? Facere cupiditate culpa magnam animi corrupti laborum possimus.</p>
-                        </div>
+                        <Slide bottom>
+                            <div className='flex flex-col md:flex-row items-center justify-between mb-5 '>
+                                <h2 className='text-4xl'>Leadership</h2>
+                                <p className="inline-block align-middle p-4 text-justify text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum suscipit, temporibus, tempora nobis deleniti fuga sunt quia unde voluptates doloribus culpa rerum? Facere cupiditate culpa magnam animi corrupti laborum possimus.</p>
+                            </div>
+                        </Slide>
+                        <Slide bottom>
+                            <div className='flex  flex-col md:flex-row items-center justify-between mb-5'>
+                                <h2 className='text-4xl '>Team,Work</h2>
+                                <p className="inline-block align-middle p-4 text-justify text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum suscipit, temporibus, tempora nobis deleniti fuga sunt quia unde voluptates doloribus culpa rerum? Facere cupiditate culpa magnam animi corrupti laborum possimus.</p>
+                            </div>
+                        </Slide>
+                        <Slide bottom>
+                            <div className='flex  flex-col md:flex-row items-center justify-between'>
+                                <h2 className='text-4xl'>Flexibility</h2>
+                                <p className=" inline-block align-middle p-4 text-justify ml-0 sm:ml-6 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum suscipit, temporibus, tempora nobis deleniti fuga sunt quia unde voluptates doloribus culpa rerum? Facere cupiditate culpa magnam animi corrupti laborum possimus.</p>
+                            </div>
+                        </Slide>
                     </div>
-                    <div className='w-full p-4'>
-                        <img className=' w-full' src="https://i.ibb.co/hVqxCGS/download.jpg" alt="" />
-                    </div>
+                    <Slide bottom>
+                        <div className='w-full p-4'>
+                            <img className=' w-full' src="https://i.ibb.co/hVqxCGS/download.jpg" alt="" />
+                        </div>
+                    </Slide>
                 </div>
-                <p className="inline-block align-middle text-justify mt-4 p-3 sm:pl-40 sm:pr-40">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown print took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.
-                </p>
+                {/* <p className="inline-block align-middle text-justify mt-4 p-3 sm:pl-40 sm:pr-40">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown print took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.
+                </p> */}
             </div>
             {/* ==============================Progress Bar  ================= */}
 
@@ -65,29 +79,6 @@ const WebDesign = () => {
 
                 </div>
             </div>
-
-
-            {/* <div>
-            <h2 className="text-left  text-white font-black">HTML</h2>
-           <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
-                
-                <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width:"95%"}}> 95%</div>
-            </div>
-           </div> */}
-
-            {/* <div className='mt-24'>
-                <ProgressBar progress={progress} />
-                <br />
-                <div >
-                    <button className='btn btn-ghost' onClick={() => setProgress(95)}>HTML</button>
-                    <button className='btn btn-ghost' onClick={() => setProgress(80)}>CSS</button>
-                    <button className='btn btn-ghost' onClick={() => setProgress(70)}>BOOSTRAP</button>
-                    <button className='btn btn-ghost' onClick={() => setProgress(50)}>TAILWIND</button>
-                    <button className='btn btn-ghost' onClick={() => setProgress(90)}>PHOTOSHOP</button>
-                    <button className='btn btn-ghost' onClick={() => setProgress(70)}>ILLUSTRATOR</button>
-                </div>
-            </div> */}
-            {/* ============================== packages =================== */}
 
             <div className="container w-full mx-auto py-20">
                 <h3 className="text-5xl text-white">My Service Planing</h3>
@@ -154,31 +145,37 @@ const WebDesign = () => {
                 </Fade>
             </div>
             {/*======== Portfulio =============== */}
-            <div className='grid grid-cols-1 sm:grid-cols-3 place-items-center mb-36 gap-4'>
-                <div class="group relative w-80">
-                    <img class="w-full object-cover" src="https://i.ibb.co/8s6BN0m/5810609.png" />
-                    <div
-                        class="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-indigo-700 opacity-0 group-hover:h-full group-hover:opacity-100 duration-500">
-                        <h1 class="text-2xl text-white">Dashboard Design</h1>
-                        <a class="mt-5 px-8 py-3 rounded-full bg-amber-400 hover:bg-amber-600 duration-300" href="#">Get it</a>
+            <div className='container w-full md:w-5/6 mx-auto grid grid-cols-1 sm:grid-cols-3 place-items-center mb-36 gap-4'>
+                <Slide bottom>
+                    <div class="group relative w-80">
+                        <img class="w-full object-cover" src="https://i.ibb.co/8s6BN0m/5810609.png" />
+                        <div
+                            class="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-indigo-700 opacity-0 group-hover:h-full group-hover:opacity-100 duration-500">
+                            <h1 class="text-2xl text-white">Dashboard Design</h1>
+                            <a class="mt-5 px-8 py-3 rounded-full bg-amber-400 hover:bg-amber-600 duration-300" href="#">Get it</a>
+                        </div>
                     </div>
-                </div>
-                <div class="group relative w-80">
-                    <img class="w-full object-cover" src="https://i.ibb.co/pjqRNzZ/5806461.png" />
-                    <div
-                        class="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-indigo-700 opacity-0 group-hover:h-full group-hover:opacity-100 duration-500">
-                        <h1 class="text-2xl text-white">Awosome Design</h1>
-                        <a class="mt-5 px-8 py-3 rounded-full bg-amber-400 hover:bg-amber-600 duration-300" href="#">Get it</a>
+                </Slide>
+                <Slide top>
+                    <div class="group relative w-80">
+                        <img class="w-full object-cover" src="https://i.ibb.co/pjqRNzZ/5806461.png" />
+                        <div
+                            class="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-indigo-700 opacity-0 group-hover:h-full group-hover:opacity-100 duration-500">
+                            <h1 class="text-2xl text-white">Awosome Design</h1>
+                            <a class="mt-5 px-8 py-3 rounded-full bg-amber-400 hover:bg-amber-600 duration-300" href="#">Get it</a>
+                        </div>
                     </div>
-                </div>
-                <div class="group relative w-80">
-                    <img class="w-full object-cover" src="https://i.ibb.co/YNv6Tvc/01.png" />
-                    <div
-                        class="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-indigo-700 opacity-0 group-hover:h-full group-hover:opacity-100 duration-500">
-                        <h1 class="text-2xl text-white">Mobile Responsive Design</h1>
-                        <a class="mt-5 px-8 py-3 rounded-full bg-amber-400 hover:bg-amber-600 duration-300" href="#">Get it</a>
+                </Slide>
+                <Slide bottom>
+                    <div class="group relative w-80">
+                        <img class="w-full object-cover" src="https://i.ibb.co/YNv6Tvc/01.png" />
+                        <div
+                            class="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-indigo-700 opacity-0 group-hover:h-full group-hover:opacity-100 duration-500">
+                            <h1 class="text-2xl text-white">Mobile Responsive Design</h1>
+                            <a class="mt-5 px-8 py-3 rounded-full bg-amber-400 hover:bg-amber-600 duration-300" href="#">Get it</a>
+                        </div>
                     </div>
-                </div>
+                </Slide>
 
             </div>
         </div>

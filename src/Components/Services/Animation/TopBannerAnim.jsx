5297cdca13fc5baab0bc51107c15495280bfe802
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-04-04 17:58:14 +0600

    add banner text

--- src/Components/Services/Animation/TopBannerAnim.jsx
+++ src/Components/Services/Animation/TopBannerAnim.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 
 const TopBannerAnim = () => {
     return (
-        <div className='flex flex-col md:flex-row justify-between items-center mt-14 overflow-hidden'>
-            <div className='w-full md:w-3/6 p-0 sm:p-5'>
-                <h2 className='text-center text-2xl sm:text-5xl sm:text-left'>leading provider of
-                    UI/UX design development
-                    services</h2>
-                <p className='text-center sm:text-left pt-3'>Emotion continues to place a lot of emphasis on UI/UX Design development. For platforms
-                    such as Android, iOS, desktops, and others, we create UI/UX. We rely on our technically
-                    proficient skilled developers to be able to comprehend your ideas and give them life. To meet
-                    the demands of each project, we provide cutting-edge research, HCD UI & UX design, and
-                    application development. For examples of dozens of unique and effective UI/UX for Android,
-                    iOS, desktop, and other platforms, visit our portfolio.</p>
+        <div className='flex flex-col md:flex-row justify-between items-center gap-3 mt-14 overflow-hidden'>
+            <div className='w-full md:w-3/6 p-0 '>
+            <img draggable="false" style={{ animation: `bounce-in 20s infinite`}} className='w-16 mx-auto flex items-start ' src="https://wgl-demo.net/zeptech/wp-content/uploads/2021/07/vector-img-2-1.png" alt="" />
+            <div className="">
+                <div className="">
+                    <h3 className="text-[150px] uppercase font-bold text-[#1E1E1E]">Digital</h3>
+                    <Fade bottom>
+                    <h4 className="text-3xl md:text-5xl font-bold text-white uppercase text-start -mt-24 tracking-[7px] leading-10">Measurable & Effective Results for Customers</h4>
+                    <p className="text-gray-200 text-xl font-thin pt-5 text-start">Your Business is Creative Idea to Launching.</p>
+                    </Fade>
+                </div>
+            </div>
             </div>
             <div className='w-full relative md:w-3/6 mx-auto p-4'>
-            <div draggable="false" style={{animation: `bounce-in 5s linear`,transform: `rotate(${Math.floor(Math.random() * (361))}deg)`}} className="md:bg-[url('https://wgl-demo.net/zeptech/wp-content/uploads/2021/07/3d-handhold-phone-mobile.png')] relative rounded-3xl flex flex-col justify-center items-center w-full mx-auto h-[80vh] md:h-[90vh] bg-cover p-7">
+            <div draggable="false" style={{animation: `bounce-in 5s linear`}} className="md:bg-[url('https://wgl-demo.net/zeptech/wp-content/uploads/2021/07/3d-handhold-phone-mobile.png')] relative rounded-3xl flex flex-col justify-center items-center w-full mx-auto h-[80vh] md:h-[90vh] bg-cover p-7">
                 <img draggable="false" style={{ animation: `bounce2 15s infinite`}} className='w-2/6 absolute bottom-20 -left-3' src="https://wgl-demo.net/zeptech/wp-content/uploads/2021/07/coub-img-1.png" alt="" />
                 </div>
             </div>

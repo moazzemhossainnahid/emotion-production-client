a85b5a2da441a25efa96fc1bc4d3999ad8d23eeb
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-04-05 21:29:03 +0600

    add images

--- src/Components/Services/Animation/TrustedPartnersAnim.jsx
+++ src/Components/Services/Animation/TrustedPartnersAnim.jsx
@@ -3,6 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { PartnersData } from '../../../Data/PartnersData';
+import Fade from 'react-reveal/Fade';
 
 const TrustedPartnersAnim = () => {
     const settings = {
@@ -44,31 +45,35 @@ const TrustedPartnersAnim = () => {
     };
     return (
         <div className='w-full h-full md:w-5/6 mx-auto py-10'>
-            <h3 className="text-white text-2xl font-semibold text-center pb-10">JOIN THE 200+ COMPANIES TRUSTING ZEPTECH COMPANY</h3>
-            <div className="">
-                <Slider className='' {...settings}>
-                    {
-                        PartnersData?.map((item) => {
-                            return (
-                                <div key={item.alt} className="py-10 d-flex justify-content-center align-items-center gap-5">
-                                    <div style={{ position: 'relative', width: '150px', height: '150px', }} className="d-flex justify-content-center mx-auto">
-                                        <img
-                                            style={{ filter: "grayscale(100%)", objectFit: 'cover' }}
-                                            className='d-flex justify-content-center align-items-center py-5 px-7'
-                                            src={item?.imageURL}
-                                            title={item?.alt}
-                                            alt={item?.alt}
-                                            fill={true}
-                                            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                        />
+            <Fade bottom duration={2000}>
+                <h3 className="text-white text-2xl font-semibold text-center pb-10">JOIN THE 200+ COMPANIES TRUSTING ZEPTECH COMPANY</h3>
+            </Fade>
+            <Fade bottom duration={2000}>
+                <div className="">
+                    <Slider className='' {...settings}>
+                        {
+                            PartnersData?.map((item) => {
+                                return (
+                                    <div key={item.alt} className="py-10 d-flex justify-content-center align-items-center gap-5">
+                                        <div style={{ position: 'relative', width: '150px', height: '150px', }} className="d-flex justify-content-center mx-auto">
+                                            <img
+                                                style={{ filter: "grayscale(100%)", objectFit: 'cover' }}
+                                                className='d-flex justify-content-center align-items-center py-5 px-7'
+                                                src={item?.imageURL}
+                                                title={item?.alt}
+                                                alt={item?.alt}
+                                                fill={true}
+                                                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                            />
+                                        </div>
                                     </div>
-                                </div>
 
-                            )
-                        })
-                    }
-                </Slider>
-            </div>
+                                )
+                            })
+                        }
+                    </Slider>
+                </div>
+            </Fade>
         </div>
     );
 };
